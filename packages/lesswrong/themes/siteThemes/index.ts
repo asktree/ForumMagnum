9c9c5c73b580851c6a6a9e3a7b1e7268cd4d9e39
agrippa kellum <ask252@cornell.edu>
2023-12-27 19:00:42 -0500

    init new forum type

--- packages/lesswrong/themes/siteThemes/index.ts
+++ packages/lesswrong/themes/siteThemes/index.ts
@@ -1,15 +1,16 @@
-import { ForumTypeString } from '../../lib/instanceSettings';
-import { alignmentForumTheme } from './alignmentForumTheme'
-import { eaForumTheme } from './eaTheme'
-import { lessWrongTheme } from './lesswrongTheme'
+import { ForumTypeString } from "../../lib/instanceSettings";
+import { alignmentForumTheme } from "./alignmentForumTheme";
+import { eaForumTheme } from "./eaTheme";
+import { lessWrongTheme } from "./lesswrongTheme";
 
 export const getSiteTheme = (forumType: ForumTypeString): SiteThemeSpecification => {
   const forumThemes: Record<ForumTypeString, SiteThemeSpecification> = {
     AlignmentForum: alignmentForumTheme,
     EAForum: eaForumTheme,
     LessWrong: lessWrongTheme,
-  }
+    FarmedAnimalWelfare: lessWrongTheme,
+  };
   if (!forumThemes[forumType]) throw Error(`No theme for forum type ${forumType}`);
 
   return forumThemes[forumType];
-}
+};
